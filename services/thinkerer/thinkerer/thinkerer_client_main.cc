891b41f4da97e4e34ad43784d09c14893a6cce96
Nikolay Zhuravlev <znick@znick.ru>
2018-04-08 13:38:57 +0500

    thinkerer is now thinkerer

---
+++ services/thinkerer/thinkerer/thinkerer_client_main.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include "thinkerer_client.h"
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+
+  // Instantiate the client. It requires a channel, out of which the
+  // actual RPCs are created. This channel models a connection to an
+  // endpoint (in this case, localhost at port 50051). We indicate
+  // that the channel isn't authenticated (use of
+  // InsecureCredentials()).
+
+  po::options_description desc("Allowed options");
+
+  desc.add_options()
+            ("help", "Helo message")
+            ("send", "send message")
+            ("recv", "recive messages")
+            ("uid", po::value<std::string>(), "my uid")
+            ("to", po::value<std::string>(), "send message to uid")
+            ("message", po::value<std::string>(), "send message body")
+        ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm); 
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  ThinkererClient greeter(
+      grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+
+  if (vm.count("send")) {
+    greeter.SendMessage(vm["uid"].as<std::string>(), vm["to"].as<std::string>(), vm["message"].as<std::string>());
+    std::cerr << "Sent!" << std::endl;
+  }
+
+  if (vm.count("recv")) {
+    const auto& uid = vm["uid"].as<std::string>();
+    std::cerr << "Recv messages for " << uid << std::endl;
+    const auto& msgs = greeter.RecvMessages(vm["uid"].as<std::string>());
+    for (const auto& m : msgs) {
+      std::cout << m.from() << "\t" << m.to() << "\t" << m.message() << std::endl;
+    }
+  }
+
+  return 0;
+}
---
+++ services/thinkerer/thinkerer/thinkerer_server.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include <grpc++/grpc++.h>
+
+#include "thinkerer/proto/thinkerer.pb.h"
+#include "thinkerer/proto/thinkerer.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using thinkerer::Msg;
+using thinkerer::MsgReq;
+using thinkerer::Msgs;
+using thinkerer::MsgReply;
+using thinkerer::Thinkerer;
+
+class MessangerImlp final : public Thinkerer::Service {
+  Status SendMessage(ServerContext* context, const Msg* request,
+                     MsgReply* reply) override 
+  {
+    messages_[request->to()].push_back(*request);
+    return Status::OK;
+  }
+
+  Status RecvMessages(ServerContext* context, const MsgReq* request,
+                      Msgs* reply) override
+  {
+    std::cerr << "RecvMessages!" << std::endl;
+    const auto messages_it = messages_.find(request->uid());
+    if (messages_it == messages_.end()) {
+      std::cerr << "RecvMessages:" << request->uid() << " 0 messages" << std::endl; 
+      return Status::OK;
+    }
+
+    std::cerr << "RecvMessages:" << request->uid() << "  " << messages_it->second.size() << " messages" << std::endl;
+    for (const auto& message : messages_it->second) {
+      auto m = reply->add_messages();
+      m->MergeFrom(message);
+    }
+    return Status::OK;
+  }
+private:
+  std::unordered_map<std::string, std::vector<Msg>> messages_;
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  MessangerImlp service;
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+
+  return 0;
+}
