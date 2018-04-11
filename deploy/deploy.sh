0b701667cdaefad9ee71a3d991d97862abc85eba
Melon <integrex@yandex.ru>
2018-04-11 23:40:44 +0500

    add network settings

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,13 @@
-sudo sed 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub | sudo tee /etc/default/grub
 sudo rm /etc/apt/sources.list
-sudo apt-get update
 sudo cp sources.list /etc/apt/sources.list
-sudo apt-get install -y nginx libltdl7
-wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
-sudo dpkg -i docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
-sudo cp docker-compose /usr/bin
-sudo python3 deploy_services.py
+sudo apt-get update
+sudo apt-get install -y nginx libltdl7 gcc make perl
+sudo mount /dev/cdrom /mnt
+sudo /mnt/VBoxLinuxAdditions.run
+sudo cloud-init.sh
+sudo rename-interface.sh
+
+# wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
+# sudo dpkg -i docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
+# sudo cp docker-compose /usr/bin
+# sudo python3 deploy_services.py
