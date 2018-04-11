2cae90fe4a34b4fdd91c6135047c5e7ebf5c5869
Melon <integrex@yandex.ru>
2018-04-11 15:19:11 +0500

    add sources mirror

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,6 @@
+sudo rm /etc/apt/sources.list
+sudo apt-get update
+sudo cp sources.list /etc/apt/sources.list
 sudo apt-get install -y nginx libltdl7
 wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
 sudo dpkg -i docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
