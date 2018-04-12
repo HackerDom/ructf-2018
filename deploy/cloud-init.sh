798ef025ed98780474a702512cf6bef8f0b9530b
Melon <integrex@yandex.ru>
2018-04-13 01:52:54 +0500

    add new version of cloud-init.sh

--- deploy/cloud-init.sh
+++ deploy/cloud-init.sh
@@ -8,9 +8,9 @@ if VBoxControl --nologo guestproperty get team; then
   echo "auto eth0" > /etc/network/interfaces.d/eth0.cfg
   echo "iface eth0 inet static" >> /etc/network/interfaces.d/eth0.cfg
   echo "dns-nameservers 8.8.8.8 8.8.4.4" >> /etc/network/interfaces.d/eth0.cfg
-  echo "address 10.$((60 + TEAM / 256)).$((TEAM % 256)).2" >>  /etc/network/interfaces.d/eth0.cfg
+  echo "address 10.$((60 + TEAM / 256)).$((TEAM % 256)).1" >>  /etc/network/interfaces.d/eth0.cfg
   echo "netmask 255.255.255.0" >>  /etc/network/interfaces.d/eth0.cfg
-  echo "gateway 10.$((60 + TEAM / 256)).$((TEAM % 256)).1" >>  /etc/network/interfaces.d/eth0.cfg
+  echo "gateway 10.$((60 + TEAM / 256)).$((TEAM % 256)).254" >>  /etc/network/interfaces.d/eth0.cfg
 else
   echo "auto eth0" > /etc/network/interfaces.d/eth0.cfg
   echo "iface eth0 inet dhcp" >> /etc/network/interfaces.d/eth0.cfg
