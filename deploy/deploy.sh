894e8c6036aafec0e1758acfda0a539d1f9dca7b
Melon <integrex@yandex.ru>
2018-04-12 15:43:46 +0500

    add cloud-init.service

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,23 +1,14 @@
+#!/bin/bash
 # sudo rm /etc/apt/sources.list
 # sudo cp sources.list /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y nginx libltdl7 gcc make perl
+sudo cp cloud-init.service /lib/systemd/system/
+sudo systemctl enable cloud-init
 sudo mount /dev/cdrom /mnt
 sudo /mnt/VBoxLinuxAdditions.run
 sudo ./cloud-init.sh
 sudo ./rename-interface.sh
-sudo sed -i 's/enp8s0/eth0/g' /etc/network/interfaces 
+sudo sed -i 's/enp8s0/eth0/g' /etc/network/interfaces
+sudo reboot
 
-# sudo touch continue-deploy.sh
-
-sudo touch /etc/rc.local
-sudo echo '#!/bin/sh -e'>/etc/rc.local
-
-sudo echo "sudo `pwd`/deploy-docker-containers.sh">>/etc/rc.local
-sudo echo "exit 0">>/etc/rc.local
-sudo chmod +x /etc/rc.local
-
-# wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
-# sudo dpkg -i docker-ce_17.03.2~ce-0~ubuntu-xenial_amd64.deb
-# sudo cp docker-compose /usr/bin
-# sudo python3 deploy_services.py
