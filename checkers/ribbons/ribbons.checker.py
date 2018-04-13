3d71dd24b23e554661f4499ae88a9ee7e7f8c6ac
AZ404 <ztbzak1@rambler.ru>
2018-04-13 12:46:51 +0000

    added ribbons checker

---
+++ checkers/ribbons/ribbons.checker.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import sys
+import checker_api
+import random
+import json
+import requests
+import base64
+from ribbons_api import Ribbons, UnexpectedStatusError, BadResponseError
+
+MAX_POSTS = 3
+
+def get_channel_name():
+	return checker_api.get_rand_string(random.randint(10, 15))
+
+def get_password():
+	return checker_api.get_rand_string(random.randint(10, 14))
+
+def get_post():
+	return checker_api.get_rand_string(random.randint(30, 120)).encode()
+
+def handle_check(hostname):
+	ribbons = Ribbons(hostname)
+
+	for i in range(2):
+		name = get_channel_name()
+		password = get_password()
+
+		cid = ribbons.add_channel(name, password)
+
+		posts = [get_post() for _ in range(random.randint(0, MAX_POSTS))]
+
+		for post in posts:
+			ribbons.add_post(cid, password, post)
+
+		new_password = get_password()
+		ribbons.change_password(cid, password, new_password)
+		password = new_password
+
+		key = ribbons.get_key(cid, password)
+
+		channel = ribbons.view(cid)
+
+		channel.decrypt(key)
+		if channel.posts != posts:
+			raise BadResponseError("Posts don't match: expected {}, actual {}".format(posts, channel.posts))
+
+	checker_api.ok()
+
+def handle_put(hostname, id, flag):
+	ribbons = Ribbons(hostname)
+
+	name = get_channel_name()
+	password = get_password()
+
+	cid = ribbons.add_channel(name, password)
+
+	key = ribbons.get_key(cid, password)
+
+	posts = [get_post() for _ in range(random.randint(0, MAX_POSTS - 1))] + [flag]
+	random.shuffle(posts)
+
+	for post in posts:
+		ribbons.add_post(cid, password, post)
+
+	credentials = {
+		"channel_id": cid,
+		"key": key.decode("latin-1")
+	}
+
+	checker_api.ok(message=base64.b64encode(json.dumps(credentials).encode()).decode())
+
+def handle_get(hostname, id, flag):
+	ribbons = Ribbons(hostname)
+	credentials = json.loads(base64.b64decode(id).decode())
+	
+	channel = ribbons.view(credentials["channel_id"])
+	channel.decrypt(credentials["key"].encode("latin-1"))
+
+	if flag.encode() not in channel.posts:
+		checker_api.corrupt(error='Flag not found. Posts count: {}'.format(len(channel.posts)))
+	else:
+		checker_api.ok()
+
+
+def main():
+	checker = checker_api.Checker(handle_check, [(handle_put, handle_get)])
+	try:
+		checker.process(sys.argv)
+	except UnexpectedStatusError as e:
+		checker_api.mumble(message=str(e))
+	except BadResponseError as e:
+		checker_api.mumble(error=str(e))
+	except requests.RequestException as e:
+		checker_api.down(exception=e)
+
+if __name__ == "__main__":
+	main()
---
+++ checkers/ribbons/ribbons_api.py
@@ -0,0 +1,108 @@
+import requests
+import struct
+import random
+import sys
+import re
+from time import sleep
+from io import BytesIO
+import user_agents
+
+KEY_SIZE = 128
+
+class UnexpectedStatusError(Exception):
+	pass
+
+class BadResponseError(Exception):
+	pass
+		
+		
+class Channel:
+	def __init__(self, name, posts):
+		self.name = name
+		self.posts = posts
+
+	def decrypt(self, key):
+		self.posts = [self._decrypt_post(post, key) for post in self.posts]
+
+	def _decrypt_post(self, post, key):
+		return bytes(post[i] ^ key[i % len(key)] for i in range(len(post)))
+	
+	@staticmethod
+	def parse(serialized):
+		bytesio = BytesIO(serialized)
+		name = _read_string(bytesio)
+		posts = []
+		while bytesio.tell() != len(serialized):
+			posts.append(_read_string(bytesio))
+		return Channel(name, posts)
+
+	
+def _read_string(bytesio):
+	size = _read_size_t(bytesio)
+	data = _read_exactly(bytesio, size)
+	return data
+
+def _read_size_t(bytesio):
+	return struct.unpack("<q", _read_exactly(bytesio, 8))[0]
+
+def _read_exactly(bytesio, count):
+	data = bytesio.read(count)
+	if len(data) != count:
+		raise EOFError()
+	return data
+
+
+class Ribbons:
+	def __init__(self, hostname):
+		self.hostname = hostname
+		self._session = None
+
+	@property
+	def session(self):
+		if not self._session or random.randint(1, 3) == 1:
+			self._session = requests.session()
+			self._session.headers = {"User-Agent": user_agents.get()}
+		return self._session
+
+	def _call(self, http_method, api_method, channel_id=None, data=None):
+		params = { "channel_id": channel_id } if channel_id else {}
+		sleep(random.randint(0,3) + random.random())
+		return self.session.request(http_method, "http://{}/{}".format(self.hostname, api_method), params=params, data=data)
+
+	def _assert_status(self, response, status):
+		if response.status_code != status:
+			raise UnexpectedStatusException("{} {}".format(response.status_code, response.reason))
+
+	def add_channel(self, name, password):
+		response = self._call("POST", "add_channel", data={ "name": name, "password": password })
+		self._assert_status(response, 201)
+		match = re.fullmatch("id:(\d+)", response.text)
+		if not match:
+			raise BadResponseError("Id not found.")
+		return match.group(1)
+
+	def add_post(self, channel_id, password, text):
+		response = self._call("POST", "add_post", channel_id, { "password": password, "text": text })
+		self._assert_status(response, 201)
+		return True
+
+	def get_key(self, channel_id, password):
+		response = self._call("GET", "key", channel_id, data={ "password": password })
+		self._assert_status(response, 200)
+		if len(response.content) != KEY_SIZE:
+			raise BadResponseError("Key size do not match expected size.")
+		return response.content
+
+	def change_password(self, channel_id, password, new_password):
+		response = self._call("POST", "change_password", channel_id, data={ "password": password, "new_password": new_password })
+		self._assert_status(response, 200)
+		return True
+
+	def view(self, channel_id):
+		response = self._call("GET", "view", channel_id)
+		self._assert_status(response, 200)
+		try:
+			channel = Channel.parse(response.content)
+		except EOFError:
+			raise BadResponseError("Channel parsing failed.")
+		return channel
