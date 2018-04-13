60b48f9d7d89903d2fc3eeb8bf220a890251e943
Melon <integrex@yandex.ru>
2018-04-14 02:29:54 +0500

    add checker main functional

--- checkers/eloquent/main.py
+++ checkers/eloquent/main.py
@@ -1,19 +1,27 @@
 import re
 import traceback
+from copy import copy
 from hashlib import sha256
 from base64 import b64encode
 from time import time
 
 import sys
+
+import markdown
+from markdown.extensions.toc import TocExtension
 from selenium import webdriver
 
+from generators import gen_login, gen_password, gen_article_title, gen_article_content, gen_flag
 from service_api import signin, signup, post_article, get_article_content, GET_ARTICLE_URL, PORT, get_driver, \
-    click_link, get_page_of_article, get_element_text_by_id, sign_in_with_driver
+    click_link, get_page_of_article, get_element_text_by_id, sign_in_with_driver, suggest_article,\
+    get_article_id_by_cookies, get_suggestions_by_cookies, ApiException, emulate_articles_view
 
 OK, CORRUPT, MUMBLE, DOWN, CHECKER_ERROR = 101, 102, 103, 104, 110
 
 ARTICLE_PATTERN = re.compile(r'<div\sclass=\"col-md-3\">(.+)', re.DOTALL)
 TABLE_OF_CONTENTS_PATTERN = re.compile(r'<a\shref=\"#(.+)\">.{1,5}\)')
+PREGENERATED_TABLE_OF_CONTENTS_PATTERN = re.compile(r'<h[3-6]\sid=\"(.+)\"')
+FLAG_LINE_TEMPLATE = re.compile(r"<div\sclass=\"card-header\">(.+)</div>")
 
 
 def print_to_stderr(*args):
@@ -22,10 +30,6 @@ def print_to_stderr(*args):
 
 def info():
     print("vulns: 1")
-    exit(OK)
-
-
-def check(hostname):
     exit(OK)
 
 
@@ -42,55 +46,105 @@ def get_article_hash(table_of_contents):
     return get_base_of_hash_of_string(','.join(table_of_contents))
 
 
-def get_article_table_of_contents(host, cookies, article_id):
-    article_content = get_article_content(host, cookies, article_id)
-    article_content = re.findall(ARTICLE_PATTERN, article_content)[0]
-    return re.findall(TABLE_OF_CONTENTS_PATTERN, article_content)
-
-
-def check_article_contents(table_of_contents, article_hash):
-    assert get_article_hash(table_of_contents) == article_hash or True
+def get_article_table_of_contents(article_content, pregen=False):
+    pattern = PREGENERATED_TABLE_OF_CONTENTS_PATTERN if pregen else TABLE_OF_CONTENTS_PATTERN
+    return re.findall(pattern, article_content)
 
 
 def check_article_js(driver, host, article_id, table_of_contents):
     get_page_of_article(driver, host, article_id)
     for header in table_of_contents:
         click_link(driver, "#" + header)
-        assert get_element_text_by_id(driver, 'mid-text') == 'Current subtitle: {}'.format(header)
+        if get_element_text_by_id(driver, 'mid-text') != 'Current subtitle: {}'.format(header):
+            exit(MUMBLE)
 
 
-# def check_():
-#     driver = get_driver()
-#
-#     for article_id in range(1, 100):
-#         cookies = signin(HOST, 'hello', '1234')
-#
-#         table_of_contents = get_article_table_of_contents(HOST, cookies, article_id)
-#         check_article_contents(table_of_contents, "1")
-#
-#         sign_in_with_driver(driver, HOST, 'hello', '1234')
-#         check_article_js(driver, HOST, article_id, table_of_contents)
-#         print("{}: OK".format(article_id))
+def check(host):
+    driver = get_driver()
+    username = gen_login()
+    password = gen_password()
+    cookies = signup(host, username, password)
+    post_article(host, cookies, gen_article_title(), gen_article_content())
+    article_id = get_article_id_by_cookies(host, cookies, from_suggestions=False)
+    article_content = get_article_content(host, cookies, article_id)
+    table_of_contents = get_article_table_of_contents(article_content)
+    sign_in_with_driver(driver, host, username, password)
+    check_article_js(driver, host, article_id, table_of_contents)
+    driver.close()
 
 
-def put(hostname, flag_id, flag, vuln):
-    pass
+def get_flag_by_cookies(host, cookies):
+    suggestions_content = get_suggestions_by_cookies(host, cookies)
+    matching = re.search(FLAG_LINE_TEMPLATE, suggestions_content)
+    if matching is None:
+        print_to_stderr("No matching with flag pattern, hostname: {}".format(host))
+        exit(CORRUPT)
+    if len(matching.groups()) == 0:
+        print_to_stderr("Empty matching with flag pattern, hostname: {}".format(host))
+        exit(CORRUPT)
+    return matching.group(1)
 
 
-def get(hostname, flag_id, flag, _):
-    pass
+def put(host, flag_id, flag, vuln):
+    username = gen_login()
+    password = gen_password()
+    cookies = signup(host, username, password)
+    article_title = flag
+    article_content = gen_article_content()
+    html_content = markdown.markdown(article_content, safe_mode='escape', extensions=[TocExtension(baselevel=3)])
+    suggest_article(host, cookies, article_title, article_content, username)
+    article_id = get_article_id_by_cookies(host, cookies)
+    table_of_contents = get_article_table_of_contents(html_content, pregen=True)
+    return ','.join([username, password, article_id, get_article_hash(table_of_contents)])
+
+
+def get(host, flag_id, flag, vuln):
+    username, password, article_id, article_hash = flag_id.split(',')
+    cookies = signin(host, username, password)
+    table_of_contents = get_article_table_of_contents(get_article_content(host, cookies, article_id))
+    real_article_hash = get_article_hash(table_of_contents)
+    if article_hash != real_article_hash:
+        print_to_stderr('expected article_hash="{}"\nreal article_hash="{}"'.format(article_hash, real_article_hash))
+        exit(MUMBLE)
+    real_flag = get_flag_by_cookies(host, cookies)
+    if real_flag != flag:
+        print_to_stderr('expected flag={}\nreal flag={}'.format(flag, real_flag))
+        exit(CORRUPT)
+    driver = get_driver()
+    emulate_articles_view(driver, host, username, password)
 
 
 COMMANDS = {'check': check, 'put': put, 'get': get, 'info': info}
 
 
-def main():
+def main_():
     try:
         COMMANDS.get(sys.argv[1], not_found)(*sys.argv[2:])
     except Exception:
         traceback.print_exc()
         exit(CHECKER_ERROR)
 
+
+def main():
+    try:
+        print('OK, CORRUPT, MUMBLE, DOWN, CHECKER_ERROR = 101, 102, 103, 104, 110')
+        # host = "10.60.1.1"
+        host = "0.0.0.0"
+        # check(host)
+        # flag = gen_flag()
+        # flag_id = put(host, 'kek', flag, 1)
+        # get(host, flag_id, flag, 1)
+        username = gen_login()
+        password = gen_password()
+        print(username, password)
+        put(host, '', '=FLAG', '')
+        driver = get_driver()
+        emulate_articles_view(driver, host, username, password)
+        driver.close()
+
+    except ApiException as e:
+        print_to_stderr(e.exc_message)
+        print(e.exc_type)
 
 if __name__ == '__main__':
     main()
--- checkers/eloquent/service_api.py
+++ checkers/eloquent/service_api.py
@@ -1,8 +1,11 @@
 import json
+from enum import Enum
 from time import sleep
 
+import re
 import requests
 from requests.cookies import RequestsCookieJar
+from requests.exceptions import ConnectTimeout, ConnectionError
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -15,49 +18,101 @@ CREATE_ARTICLE_URL = "http://{host}:{port}/create"
 REGISTER_URL = "http://{host}:{port}/register"
 LOGIN_URL = "http://{host}:{port}/signin"
 POST_ARTICLE_URL = "http://{host}:{port}/post-article"
+SUGGEST_ARTICLE_URL = "http://{host}:{port}/post-article?user={user}"
 GET_ARTICLE_URL = "http://{host}:{port}/article/{article_id}"
 SET_VAL_BY_NAME_SCRIPT_TEMPLATE = """$('[name="{}"]').val("{}")"""
+MY_SUGGESTIONS = "http://{host}:{port}/suggestions"
+MY_ARTICLES = "http://{host}:{port}/my-articles"
+TABLE_OF_CONTENTS_PATTERN = re.compile(r'<a\shref=\"#(.+)\">.{1,5}\)')
+
+ARTICLE_BTN_TEMPLATE = re.compile(r'<a\shref=\"/article/(\d+)\"\sclass=\"btn\sbtn-primary\sbottom-btn\"')
+ARTICLE_ID_TEMPLATE = re.compile(r"/article/(\d+)")
+
+TIMEOUT = 2
+
+
+class ExceptionType(Enum):
+    MUMBLE = 103
+    DOWN = 104
+
+
+class ApiException(Exception):
+    def __init__(self, exc_type, exc_message):
+        self.exc_type = exc_type
+        self.exc_message = exc_message
+
+
+def api_method(func):
+    def patched_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except requests.ConnectionError as e:
+            raise ApiException(ExceptionType.DOWN, 'in function {}, exception: {}'.format(func, e))
+        except requests.HTTPError as e:
+            raise ApiException(ExceptionType.MUMBLE, 'in function {}, exception: {}'.format(func, e))
+    return patched_func
 
 
 def get_driver():
-    driver = webdriver.PhantomJS(service_log_path='/dev/null')
-    driver.set_window_size(1600, 900)
-    return driver
+    # driver = webdriver.PhantomJS(service_log_path='/dev/null')
+    # driver.set_window_size(1600, 900)
+    # return driver
+    return webdriver.Chrome()
 
 
+@api_method
 def signup(host, login, password):
     r = requests.post(
-        url=REGISTER_URL.format(host, PORT),
+        url=REGISTER_URL.format(host=host, port=PORT),
         data={'username': login, 'password': password},
-        timeout=15
+        timeout=TIMEOUT
     )
+    r.raise_for_status()
     return r.request._cookies
 
 
+@api_method
 def signin(host, login, password):
     r = requests.post(
         url=LOGIN_URL.format(host=host, port=PORT),
         data={'username': login, 'password': password},
-        timeout=15
+        timeout=TIMEOUT
     )
+    r.raise_for_status()
     return r.request._cookies
 
 
+@api_method
 def post_article(host, cookies, title, content):
-    requests.post(
-        url=POST_ARTICLE_URL.format(host, PORT),
+    r = requests.post(
+        url=POST_ARTICLE_URL.format(host=host, port=PORT),
         cookies=cookies,
         data={'title': title, 'content': content},
-        timeout=15
+        timeout=TIMEOUT
     )
+    r.raise_for_status()
 
 
+@api_method
+def suggest_article(host, cookies, title, content, user):
+    r = requests.post(
+        url=SUGGEST_ARTICLE_URL.format(host=host, port=PORT, user=user),
+        cookies=cookies,
+        data={'title': title, 'content': content},
+        timeout=TIMEOUT
+    )
+    r.raise_for_status()
+    return True
+
+
+@api_method
 def get_article_content(host, cookies, article_id):
     r = requests.get(
         url=GET_ARTICLE_URL.format(host=host, port=PORT, article_id=article_id),
         cookies=cookies,
-        timeout=15
+        timeout=TIMEOUT
     )
+    r.raise_for_status()
     return r.content.decode()
 
 
@@ -66,6 +121,31 @@ def sign_in_with_driver(driver, host, login, password):
     send_keys_to_field(driver, 'username', login)
     send_keys_to_field(driver, 'password', password)
     send_keys_to_field(driver, 'password', Keys.ENTER)
+
+
+@api_method
+def get_article_id_by_cookies(host, cookies, from_suggestions=True):
+    pattern = MY_SUGGESTIONS if from_suggestions else MY_ARTICLES
+    r = requests.get(
+        url=pattern.format(host=host, port=PORT),
+        cookies=cookies,
+        timeout=TIMEOUT
+    )
+    r.raise_for_status()
+    content = r.content.decode()
+    article_id = ARTICLE_ID_TEMPLATE.search(content).group(1)
+    return article_id
+
+
+@api_method
+def get_suggestions_by_cookies(host, cookies):
+    r = requests.get(
+        url=MY_SUGGESTIONS.format(host=host, port=PORT),
+        cookies=cookies,
+        timeout=TIMEOUT
+    )
+    r.raise_for_status()
+    return r.content.decode()
 
 
 def get_page_of_article(driver, host, article_id):
@@ -87,3 +167,14 @@ def send_keys_to_field(driver, field_name, keys):
 
 def set_field_val_by_name(driver, field_name, value):
     driver.execute_script(SET_VAL_BY_NAME_SCRIPT_TEMPLATE.format(field_name, value))
+
+
+def emulate_articles_view(driver, host, username, password):
+    sign_in_with_driver(driver, host, username, password)
+    sleep(10)
+    driver.get(MY_SUGGESTIONS.format(host=host, port=PORT))
+    source = driver.page_source
+    for article_id in re.findall(ARTICLE_BTN_TEMPLATE, source):
+        driver.get(GET_ARTICLE_URL.format(host=host, port=PORT, article_id=article_id))
+        for href in re.findall(TABLE_OF_CONTENTS_PATTERN, driver.page_source):
+            click_link(driver, '#' + href)
