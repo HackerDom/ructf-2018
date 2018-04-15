af2c42f78313502636cb06f7ad637ed6de23a8f7
Melon <integrex@yandex.ru>
2018-04-15 13:46:51 +0500

    tiny checker fix

--- checkers/eloquent/checker.py
+++ checkers/eloquent/checker.py
@@ -2,6 +2,8 @@
 import paramiko
 import sys
 
+from paramiko import SSHException
+
 HOST = "10.10.10.101"
 USER = 'root'
 PASSWORD = 'gbhfncrjgbhfncrbqgbhfn'
@@ -12,17 +14,20 @@ def main():
     root_dir = '/home/ructf-2018/checkers/eloquent'
     checker_name = 'checker-server.py'
     client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=HOST, username=USER, password=PASSWORD, port=PORT, timeout=10)
-    c = client.get_transport().open_session()
-    c.exec_command("cd {}; ./{} {}".format(root_dir, checker_name, ' '.join(sys.argv[1:])))
-    stderr_data = c.recv_stderr(10240).decode()
-    stdout_data = c.recv(10240).decode()
-    print(stdout_data)
-    print(stderr_data, file=sys.stderr)
-    exit(int(c.recv_exit_status()))
-    client.close()
+    try:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=HOST, username=USER, password=PASSWORD, port=PORT, timeout=10)
+        c = client.get_transport().open_session()
+        c.exec_command("cd {}; ./{} {}".format(root_dir, checker_name, ' '.join(sys.argv[1:])))
+        stderr_data = c.recv_stderr(10240).decode()
+        stdout_data = c.recv(10240).decode()
+        print(stdout_data)
+        print(stderr_data, file=sys.stderr)
+        exit(int(c.recv_exit_status()))
+    except SSHException as e:
+        exit()
+    finally:
+        client.close()
 
 if __name__ == '__main__':
     main()
-
